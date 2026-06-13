e1dbb07c6c7a8c4e26ecea56a8e7ca8b451932b7
Vivek <vivekkr8235@zohomail.in>
2026-06-13 21:37:16 +0530

    vaxtrack: added interface for booking service

--- v2_backend/v2.0.0/Interfaces/IBookingService.cs
+++ v2_backend/v2.0.0/Interfaces/IBookingService.cs
@@ -1,25 +1,36 @@
 
 
+using Vaxtrack.Dtos.BookingDtos;
+
 namespace Vaxtrack.Interfaces
 {
     public interface IBookingService
     {
         // create booking
+        Task<CreateBookingResponseDto> CreateBookingAsync(CreateBookingRequestDto createBookingReqeust);
 
         // update booking
+        Task<UpdateBookingResponseDto> UpdateBookingAsync(UpdateBookingRequestDto updateBookingRequest);
 
         // get booking by booking id
+        Task<BookingProfileDataDto?> GetBookingByBookingIdAsync(string bookingId);
 
         // get booking by user id
+        Task<BookingProfileDataDto?> GetBookingsByUserIdAsync(string userId);
 
-        // get booking by hospital id
+        // get bookings by hospital id
+        Task<List<BookingProfileDataDto>?> GetBookingsByHospitalIdAsync(string hospitalId);
 
         // approve bookings
+        Task<BookingProfileDataDto> ApproveBookingsAsync(string bookingId);
 
         // cancel bookings
+        Task<BookingProfileDataDto> CancelBookingsAsync(string bookingId);
 
         // delete bookings
+        Task DeleteBookingAsync(string bookingId);
 
         // is booking exists
+        Task<bool> IsBookingExists(string bookingId);
     }
 }
